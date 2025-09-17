559abb53e3edb78cc287ce2eb37c588cfe31001a
Lucas <olucasbm1213@gmail.com>
2025-09-17 10:10:39 -0300

    conditions if, else

---
+++ exercicies/exercicio4.1.py
@@ -0,0 +1,6 @@
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+if a > b:
+    print("O primeiro é maior!")
+if b > a:
+    print("O segundo é maior!")
---
+++ programs/programa4.5.py
@@ -0,0 +1,6 @@
+ano_carro = int(input("Digite o ano do carro: "))
+idade_carro = 2025 - ano_carro
+if idade_carro <= 3:
+    print("Carro novo")
+else:
+    print("Carro velho")
